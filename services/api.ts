b0100f686f6015c3a9e260c1ec73afda2108acbb
Akhilesh-github-7 <akhileshmsskp@gmail.com>
2025-10-28 20:49:00 +0530

    Fix: Corrected movie details screen name in _layout.tsx

---
+++ services/api.ts
@@ -0,0 +1,65 @@
+export const TMDB_CONFIG = {
+    BASE_URL: 'https://api.themoviedb.org/3',
+    API_KEY: process.env.EXPO_PUBLIC_MOVIE_API_KEY,
+    headers: {
+        accept: 'application/json',
+        Authorization: `Bearer ${process.env.EXPO_PUBLIC_MOVIE_API_KEY}`,
+    }
+}
+
+export const fetchMovies = async({ query } : {query: string}) =>{
+    const endpoint = query ?
+    `${TMDB_CONFIG.BASE_URL}/search/movie?query= ${encodeURIComponent(query)}` : 
+    `${TMDB_CONFIG.BASE_URL}/discover/movie?sort_by=popularity.desc`;
+
+    const response = await fetch(endpoint,{
+        method: 'GET',
+        headers: TMDB_CONFIG.headers,
+    });
+
+    if(!response.ok){
+        throw new Error('Failed to fetch movies');
+    }
+
+    const data = await response.json();
+    return data.results;
+
+}
+
+export const fetchMovieDetails = async (movieId: string): Promise<MovieDetails> =>{
+    try{
+
+        const response = await fetch(`${TMDB_CONFIG.BASE_URL}/movie/${movieId}?api_key=${TMDB_CONFIG.API_KEY}`,{
+            method: 'GET',
+            headers: TMDB_CONFIG.headers,
+        });
+
+        if(!response.ok){
+            throw new Error('Failed to fetch movie details');
+        }
+        const data = await response.json();
+        return data;
+
+    }catch(error){
+
+        console.log(error);
+        throw error;
+        
+    }
+}
+
+
+
+// const url = 'https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc';
+// const options = {
+//   method: 'GET',
+//   headers: {
+//     accept: 'application/json',
+//     Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJmNDk2ZWU4MTViZjBiNWUwOWJhMjU0OTc1NzE1MWM1NSIsIm5iZiI6MTczNjA1Nzk2OC43OTksInN1YiI6IjY3N2EyNDcwODJjY2UxNWE3Njc0ZThiOCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.c--Xrfuoyswj5vNz1idsdAhbxp98LxEQQaP0Qp5q4jI'
+//   }
+// };
+
+// fetch(url, options)
+//   .then(res => res.json())
+//   .then(json => console.log(json))
+//   .catch(err => console.error(err));
